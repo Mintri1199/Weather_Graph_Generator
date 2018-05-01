8399312d1e1a25fee1a1b60e9afa86f92edfa6f1
Mintri1199 <38873896+Mintri1199@users.noreply.github.com>
2018-04-30 23:45:40 -0400

    Update read.py

--- read.py
+++ read.py
@@ -1,15 +1,16 @@
+# This is the first class of the project
 import csv
 # Access the file and marked the file so it will help when searching specific data at a specific location
 class Read():
     def __init__(self,):
         print("Welcome\nThis is a program that render a line graph of the max and min temperature "
               +"of the given weather csv file")
-        self.track = 1
-        self.locations = {}
+        self.track = 1 # The key for the dictionary
+        self.locations = {} # Dictionary of locations in the csv file
         self.file = input("Please type the csv file name: ")
         self.frmt = self.file.lower().lstrip()
 
-    # if the user enter the filename wrong
+    # checking if the user enter the filename wrong
     def filename_checking(self):
         while True:
             try:
@@ -21,10 +22,10 @@ class Read():
             except:
                 self.file = str(input('Please type the file name again\nType quit to end'))
                 self.frmt = self.file.lower().lstrip()
-
-    # Accessing, counting, storing, and displaying the number of locations in the file
+    
+    # Since ct_weather.csv file have multiple locations, this function will access, count, store 
+    # the locations, and then display them to the user so they could choose
     def access(self):
-
         if self.frmt != 'quit':
             with open(self.frmt) as f:
                 reader = csv.reader(f)
